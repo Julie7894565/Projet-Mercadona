17f02b5dd1b09467e01d40daefd79868e9376f35
Julie7894565 <julie.midon@gmail.com>
2023-04-21 17:21:33 +0200

    products page fixed

--- src/Form/Product1Type.php
+++ src/Form/Product1Type.php
@@ -7,14 +7,13 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ProductType extends AbstractType
+class Product1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('label')
             ->add('description')
-            ->add('weight')
             ->add('price')
             ->add('picture')
             ->add('productCategory')
